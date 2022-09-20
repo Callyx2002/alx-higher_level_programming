312f7adc143746c90565d0456c2ed3164faafe09
Caleb Arodu <callyx2002@gmail.com>
2022-09-20 23:59:13 +0100

    if else loops functions

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
-        print("{}{}".format(0, i), end=", ")
+    if i < 99:
+        print("{}{}".format(0, i) if i < 10 else "{}".format(i), end=", ")
     else:
-        print("{}, ".format(i), end="" if i < 99 else "{}\n".format(i))
+        print("{}".format(i))
