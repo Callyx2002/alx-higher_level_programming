f2ea06153f91cb2fd86f62fb4ed992cc0c25eedc
Caleb Arodu <callyx2002@gmail.com>
2022-09-20 12:03:17 +0100

    python hello world

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * check_cycle - checks and looks for a cycle in a linked list
+ * 
+ * @list: the first element in  the linked list
+ * Return - either 1 or 0 based on the condition met.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *temp = list;
+	listint_t *check = temp;
+
+	if (list->next == NULL)
+	{
+		return (0);
+	}
+
+	while (check && temp && temp->next)
+	{
+		temp = temp->next->next;
+		check = check->next;
+		if (check == temp)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
