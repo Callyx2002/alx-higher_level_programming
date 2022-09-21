f446a2f055dac60061d9889cf15a0c5af7890df9
Caleb Arodu <callyx2002@gmail.com>
2022-09-21 03:55:41 +0100

    If else statements and loops

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -15,20 +15,15 @@ listint_t *insert_node(listint_t **head, int number)
     }
     while (high && low)
     {
-        if ((*head)->n > number)
+        
+        if (low->n < number)
         {
-           
+            low = new;
             new->next = (*head);
-             low = new;
+            return (new);
         }
         low = low->next;
         high = low->next;
-        if (low->n < number && high->n > number)
-        {
-            low->next = new;
-            new->next = high;
-            return (new);
-        }
     }
 
     return (NULL);
