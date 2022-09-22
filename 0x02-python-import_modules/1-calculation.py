2580d78b1c55eada2a907c396522791171a116c1
Caleb Arodu <callyx2002@gmail.com>
2022-09-22 23:04:07 +0100

    import modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-import calculator_1
+from calculator_1 import add, sub, div, mul
 """
 include the if __name__ == "__main__" so my program wont run when imported
 """
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
-    print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
-    print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
-    print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
+else:
+    pass
