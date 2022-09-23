946780eed0ceaae7b05b75a8729323ba615144a0
Caleb Arodu <callyx2002@gmail.com>
2022-09-23 11:02:40 +0100

    import modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
-import calculator_1
-import sys
+from calculator_1 import add, mul, sub, div
 
 
-def main():
+def myfunc(argument):
     myList = ["+", "-", "*", "/"]
-    first = int(sys.argv[1])
-    second = int(sys.argv[3])
-    if (len(sys.argv) != 4):
+    first = int(argument[1])
+    second = int(argument[3])
+    if (len(argument) != 4):
         print("{}: {} {} {} {}".format(
             "Usage", "./100-my_calculator.py", "<a>", "operator", "<b>"))
         sys.exit(1)
-    if (sys.argv[2] not in myList):
+    if (argument[2] not in myList):
         print("{}: {}".format(
             "Unknown operator. Available operators", "+, -, * and /"))
         sys.exit(1)
 
-    if sys.argv[2] == "+":
-            result = calculator_1.add(first, second)
-            print("{} {} {} = {}".format(first, sys.argv[2], second, result))
-    if sys.argv[2] ==  "-":
-            result = calculator_1.sub(first, second)
-            print("{} {} {} = {}".format(first, sys.argv[2], second, result))
-    if sys.argv[2] ==  "/":
-            result = calculator_1.div(first, second)
-            print("{} {} {} = {}".format(first, sys.argv[2], second, result))
-    if sys.argv[2] ==  "*":
-            result = calculator_1.mul(first, second)
-            print("{} {} {} = {}".format(first, sys.argv[2], second, result))
+    if argument[2] == "+":
+        result = add(first, second)
+        print("{} {} {} = {}".format(first, argument[2], second, result))
+    if argument[2] == "-":
+        result = sub(first, second)
+        print("{} {} {} = {}".format(first, argument[2], second, result))
+    if argument[2] == "/":
+        result = div(first, second)
+        print("{} {} {} = {}".format(first, argument[2], second, result))
+    if argument[2] == "*":
+        result = mul(first, second)
+        print("{} {} {} = {}".format(first, argument[2], second, result))
 
 
 if __name__ == "__main__":
-    main()
+    import sys
+    myfunc(sys.argv)
