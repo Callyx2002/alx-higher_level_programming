5912c9c8908994efc37d6733d704e302cc9afc9b
Caleb Arodu <callyx2002@gmail.com>
2022-09-23 10:46:31 +0100

    import modules

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import calculator_1
+import sys
+
+
+def main():
+    myList = ["+", "-", "*", "/"]
+    print(sys.argv[3])
+    first = int(sys.argv[1])
+    second = int(sys.argv[3])
+    if (len(sys.argv) != 4):
+        print("{}: {} {} {} {}".format(
+            "Usage", "./100-my_calculator.py", "<a>", "operator", "<b>"))
+        sys.exit(1)
+    if (sys.argv[2] not in myList):
+        print("{}: {}".format(
+            "Unknown operator. Available operators", "+, -, * and /"))
+        sys.exit(1)
+
+    match sys.argv[2]:
+        case "+":
+            result = calculator_1.add(first, second)
+            print("{} {} {} = {}".format(first, sys.argv[2], second, result))
+        case "-":
+            result = calculator_1.sub(first, second)
+            print("{} {} {} = {}".format(first, sys.argv[2], second, result))
+        case "/":
+            result = calculator_1.div(first, second)
+            print("{} {} {} = {}".format(first, sys.argv[2], second, result))
+        case "*":
+            result = calculator_1.mul(first, second)
+            print("{} {} {} = {}".format(first, sys.argv[2], second, result))
+
+
+if __name__ == "__main__":
+    main()
