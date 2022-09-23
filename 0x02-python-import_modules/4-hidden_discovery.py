6c4867f29f3ab21eae87035ea4fb2dd2441204b3
Caleb Arodu <callyx2002@gmail.com>
2022-09-23 09:58:52 +0100

    import modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 import hidden_4
+
+
 def main():
     myList = dir(hidden_4)
     for i in myList:
-        if i[0:2] !="__":
+        if i[0:2] != "__":
             print("{:s}".format(i))
+
+
 if __name__ == "__main__":
     main()
