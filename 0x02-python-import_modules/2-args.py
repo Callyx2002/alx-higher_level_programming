32b0d0ff7005539654aae424046af9fcd9e1e67c
Caleb Arodu <callyx2002@gmail.com>
2022-09-23 09:21:12 +0100

    import modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 import sys
-print(len(sys.argv) - 1)
+if __name__ == "__main__":
+    if (len(sys.argv) == 2):
+        print("{} {}:".format((len(sys.argv) - 1), "argument"))
+    elif (len(sys.argv) == 1):
+        print("{} {}.".format((len(sys.argv) - 1), "arguments"))
+    else:
+        print("{} {}:".format((len(sys.argv) - 1), "arguments"))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
+else:
+    pass
