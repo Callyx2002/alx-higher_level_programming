02858f56aec7ecc9e50a261e89f8b66513d2e311
Caleb Arodu <callyx2002@gmail.com>
2022-09-24 19:17:42 +0100

    import modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,34 +1,24 @@
 #!/usr/bin/python3
-from calculator_1 import add, mul, sub, div
-
-
-def myfunc(argument):
-    myList = ["+", "-", "*", "/"]
-    first = int(argument[1])
-    second = int(argument[3])
-    if (len(argument) != 4):
-        print("{}: {} {} {} {}".format(
-            "Usage", "./100-my_calculator.py", "<a>", "operator", "<b>"))
-        sys.exit(1)
-    if (argument[2] not in myList):
-        print("{}: {}".format(
-            "Unknown operator. Available operators", "+, -, * and /"))
-        sys.exit(1)
-
-    if argument[2] == "+":
-        result = add(first, second)
-        print("{} {} {} = {}".format(first, argument[2], second, result))
-    if argument[2] == "-":
-        result = sub(first, second)
-        print("{} {} {} = {}".format(first, argument[2], second, result))
-    if argument[2] == "/":
-        result = div(first, second)
-        print("{} {} {} = {}".format(first, argument[2], second, result))
-    if argument[2] == "*":
-        result = mul(first, second)
-        print("{} {} {} = {}".format(first, argument[2], second, result))
-
-
 if __name__ == "__main__":
-    import sys
-    myfunc(sys.argv)
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+
+    if (len(argv)) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    
+    if argv[2] == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif argv[2] == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif argv[2] == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
+    elif argv[2] == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+
+    else:
+        print("Unknown operator. Available operator: +, -, * and /")
+        exit(1)
--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+from sys import intern
+
+
 def add(a, b):
     """My addition function
 
