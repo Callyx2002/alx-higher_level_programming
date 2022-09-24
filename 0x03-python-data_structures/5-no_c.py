dead30ad642c92954f77483c63000fd173f15cbd
Caleb Arodu <callyx2002@gmail.com>
2022-09-25 00:27:20 +0100

    Data Structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     new = ""
     for i in my_string:
-        if i =="c" or i == "C":
+        if i == "c" or i == "C":
             continue
         else:
             new += i
