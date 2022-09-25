f50363583303f743f83bda89f8dd3e30dd1747ad
Caleb Arodu <callyx2002@gmail.com>
2022-09-25 02:15:51 +0100

    Data Structures

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == "":
+    if len(sentence) == 0:
         first = None
         counting = 0
     else:
-        sentence = list(str(sentence))
+        sentence = list(sentence)
         counting = len(sentence)
         new = str(sentence[0])
         first = new[0]
-        return counting, first
+    return counting, first
