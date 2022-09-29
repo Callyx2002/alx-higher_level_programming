68a784893ed14310c49b2c91e3e37a10182b200e
Caleb Arodu <callyx2002@gmail.com>
2022-09-28 17:17:47 -0700

    More data structures tasks

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    new = {}
     for key in a_dictionary.keys():
         a_dictionary[key] *= 2
-    return a_dictionary
+        new.update({key: a_dictionary[key]})
+    return new
