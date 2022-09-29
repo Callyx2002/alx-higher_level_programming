d17996320bcc1bc2d95715bd29c71568b9fa959c
Caleb Arodu <callyx2002@gmail.com>
2022-09-28 17:57:46 -0700

    More data structures tasks

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,12 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    else:
-        return max(a_dictionary)
+    mylist = list(a_dictionary.keys())
+    biggest = None
+    for i in mylist:
+        for j in mylist[mylist.index(i) + 1:]:
+            if a_dictionary[i] > a_dictionary[j]:
+                biggest = i
+            else:
+                biggest = j
+    return biggest
